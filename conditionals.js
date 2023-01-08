d736c5a47d21b7383612ac8845f9cd80a948f579
Alexandra Zambrano <alezambrano03@gmail.com>
2023-01-08 19:43:38 +0100

    practica js

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//declarar variables para conectar con el HTML
+
+//una para el botón del dado
+//una para el botón de reset
+//una para el número que arroje el dado
+//una para el mensaje que se ve debajo del numero que saque el dado
+
+
+
+// función para lanzar el dado con numeros de 1 al 6
+
+// función para que el botón pueda lamzar el dado
+
+//función para reiniciar el juego
+
+// Darle funcionalidad al botón de reiniciar
+
---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// declarar variable usando el DOM para conectar con el html
+
+//uno para el modal
+//uno para el overlay
+//uno para el botón que abre el modal
+//uno para el botón que cierra el modal
+
+
+// crear una función que abra el modal
+
+
+
+
+//crear una función que cierre el modal
+
+
+
+
+// Usar escuchadores (evenrListeners) para que los botones funcionen
+
+
